566052713f6cb403d21a347a659e238e2bad863b
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-11 15:30:48 +0200

    add textview on Frag2 java binding frag 2 textview UI

--- app/src/main/java/com/example/tpfragmentjava/Fragment2.java
+++ app/src/main/java/com/example/tpfragmentjava/Fragment2.java
@@ -7,17 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class Fragment2 extends Fragment {
 
-    public Fragment2() {
-        // Required empty public constructor
-    }
+    public Fragment2() {}
+
+    private TextView textView;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false);
+        View view = inflater.inflate(R.layout.fragment_2,
+                container,
+                false);
+        textView = view.findViewById(R.id.frag2TextView);
+        return view;
+    }
+    public void received(String name) {
+        textView.setText(name);
     }
 }
 
