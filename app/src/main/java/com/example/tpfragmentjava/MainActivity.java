ab78826f5c3106bd8645a7d08cd59f7eba707ae3
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-11 15:51:56 +0200

    rename fragments ids

--- app/src/main/java/com/example/tpfragmentjava/MainActivity.java
+++ app/src/main/java/com/example/tpfragmentjava/MainActivity.java
@@ -1,51 +1,57 @@
 package com.example.tpfragmentjava;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements Fragment1.Communication{
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements Fragment1.Communication {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Fragment1","L'activité est créée'");
+        Log.d("Fragment1", "L'activité est créée'");
     }
+
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("Fragment1","L'activité a Starté'");
+        Log.d("Fragment1", "L'activité a Starté'");
     }
+
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("Fragment1","L'activité a Résumé'");
+        Log.d("Fragment1", "L'activité a Résumé'");
     }
+
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("Fragment1","L'activité est onPause'");
+        Log.d("Fragment1", "L'activité est onPause'");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("Fragment1","L'activité est stoppée'");
+        Log.d("Fragment1", "L'activité est stoppée'");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("Fragment1","L'activité est détruite'");
+        Log.d("Fragment1", "L'activité est détruite'");
     }
 
     @Override
     public void send(String nom) {
         TextView textView = findViewById(R.id.textViewActivity);
         textView.setText(nom);
-//        Fragment2
+        Fragment2 fragment2 = (Fragment2) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment2);
+        if (fragment2 != null)
+            fragment2.received(nom);
     }
 }
